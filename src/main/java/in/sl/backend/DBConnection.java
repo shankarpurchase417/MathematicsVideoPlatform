127a5214593fed57024c2dd31996a09e684a20df
shankarpurchase417 <shankarpurchase417@gmail.com>
2025-03-05 11:08:48 +0530

    Initial commit for Railway deployment

--- src/main/java/in/sl/backend/DBConnection.java
+++ src/main/java/in/sl/backend/DBConnection.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/math_videos";
+    private static final String URL = "jdbc:mysql://mysql.railway.internal:3306/railway";
     private static final String USER = "root";  // Change to your MySQL username
-    private static final String PASSWORD = "Shankar@1992";  // Change to your MySQL password
+    private static final String PASSWORD = "zVJJPsdigxpOjHbyIkieXFqAKikWvXDw";  // Change to your MySQL password
 
     public static Connection getConnection() {
         Connection conn = null;
